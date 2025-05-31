3f5a95af2f5cf2960ad7886104ea83c4a50d2212
Kedesdt <kedesdt@gmail.com>
2025-05-31 17:36:25 -0300

    inicial

---
+++ app/service/auth_service.py
@@ -0,0 +1,43 @@
+from flask import request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# Configurando o gerenciador de login
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+
+# Modelo de usuário (simplificado)
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# Simulando um banco de dados de usuários
+users = {
+    "admin": User(1, "admin", "senha123")
+}
+
+@login_manager.user_loader
+def load_user(user_id):
+    return next((user for user in users.values() if str(user.id) == user_id), None)
+
+# Função de autenticação
+def authenticate(username, password):
+    user = users.get(username)
+    if user and user.verify_password(password):
+        login_user(user)
+        return True
+    return False
+
+# Função de logout
+def logout():
+    logout_user()
+
+# Exemplo de rota protegida
+@login_required
+def protected_view():
+    return "Você está autenticado e pode ver esta página protegida!"
